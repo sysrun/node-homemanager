0b4b4335375dfc3d0aa93e3fb663e1d042718f2a
Frederik Granna <frederik@tape.io>
2012-12-08 21:06:25 +0100

    switched to lineparser

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var clc = require('cli-color');
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 
+
 var decoderJeelink = require("./lib/decoder/jeelink");
 
 var jeelinkSerial = new SerialPort(config.serials.jeelink.port, { 
     baudrate: config.serials.jeelink.baud,
-    //parser: serialport.parsers.readline("\n") 
-    parser: serialport.parsers.readline("-> ack") 
+    parser: serialport.parsers.readline("\n") 
+    //parser: serialport.parsers.readline("-> ack") 
 });
 
 console.log(clc.blue.bold("Starting..."));
