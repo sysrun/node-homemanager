fe6853805441d4a7e4d0cbea82f876df46147dd6
Frederik Granna <frederik@tape.io>
2012-12-11 20:53:49 +0100

    some debug

--- app.js
+++ app.js
@@ -12,12 +12,24 @@ var SerialPort = serialport.SerialPort; // localize object constructor
 modManager = require("./lib/modulemanager");
 
 modManager.init(function(){
+  console.log("--------------------------");
+  console.log(clc.bold("Modules loaded:"));
+  for(var mod in modManager.modules()) {
+    console.log(">> "+clc.blue.bold(mod)+" "+clc.blue("Type:"+modManager.modules()[mod].type));
+  }
+  console.log("--------------------------");
+  
   console.log(clc.blue.bold("Starting..."));
-  //console.log(modManager.modules())
 
   var jeelinkSerial = new SerialPort(config.serials.jeelink.port, { 
       baudrate: config.serials.jeelink.baud,
       parser: serialport.parsers.readline("\n") 
+  });
+
+
+  modManager.getEventHandler("jeelink").on("receivefailed",function(data){
+    console.log("LINEE::")
+    console.log(data);
   });
 
   jeelinkSerial.on("data", modManager.getModule("jeelink").processData);
